625fda4619bdde985e6491be88d6b77f7c80c2fb
yeliang <wx-yel@dtdream.com>
2018-10-08 14:30:46 +0800

    init

--- src/main/java/cn/leancloud/demo/todo/TodoServlet.java
+++ src/main/java/cn/leancloud/demo/todo/TodoServlet.java
@@ -37,7 +37,6 @@ public class TodoServlet extends HttpServlet {
       if (e.getCode() == AVException.OBJECT_NOT_FOUND) {
         // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo
         // 数据表还未创建，所以返回空的
-        // Todo 列表。
         // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
         req.setAttribute("todos", new ArrayList<>());
       } else {
